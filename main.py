40465c3335049b5c6af31d3242169c1d2371cf0d
Muhammad Fanik <muhammadfanik0@gmail.com>
2026-03-06 14:09:49 +0500

    Endpoints added

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from fastapi import FastAPI
 # Here, I am creating the web application. app is the main object that will handle the request from the users
 app = FastAPI()
 
+# This is the first endpoint
 @app.get("/")   # This is a decorator. It tells FastAPI that whenever someone visits the homepage(/) using GET request, run the func below
 def hello():    # This function will run
     return {"message": "Hello World"}   # This returns a dictionary. FastAPI automatically converts it into JSON
 
+
+# This is the second endpoint
 @app.get("/about")
 def about():
     return {"message": "About Page"}
