2b2f38cb9582f526bde47e55607f55b1657f6a68
Muhammad Fanik <muhammadfanik0@gmail.com>
2026-06-28 07:16:13 +0500

    Endpoints made

--- 3- Job Listings portal/main.py
+++ 3- Job Listings portal/main.py
@@ -37,3 +37,80 @@ class Job(BaseModel):
     @property
     def salary_range(self) -> str:
         return f"${self.min_salary} - ${self.max_salary}"
+
+
+# Helper function to load data
+def load_data():
+    with open("data.json", "r") as f:
+        data = json.load(f)
+    return data
+
+
+# First endpoint - Homepage
+@app.get("/")
+def home():
+    return {"message": "This is the home page for Job Listings portal"}
+
+# 2nd Endpoint - View all the jobs
+@app.get("/jobs")
+def jobs():
+    data = load_data()
+    return data
+
+# 3rd Endpoint - View a specific job by ID
+@app.get("/view_specific_job/{job_id}")
+def view_specific(job_id: int = Path(..., title="Job ID", examples=[1])):
+
+    # load the data
+    data = load_data()
+
+    # loop through every job in the data
+    for job in data:
+        if job["id"] == job_id:
+            return job
+
+    raise HTTPException(status_code=404, detail="This job does not exist")
+
+# 4th Endpoint - Search by location, min salary, max experience
+@app.get("/search")
+def search(
+        # Query param 1
+        location: str = Query(default=None, description="Location of the office where the job is available"),
+        # Query param 2
+        min_salary: float = Query(default = None, description="Minimum salary for this job role"),
+        # Query param 3
+        experience_yrs: float = Query(default=None, description="Maximum years of exp required for this job role")):
+
+    # Load the data
+    data = load_data()
+
+    all_jobs = []
+
+    for job in data:
+        if location is not None and job["location"] != location:
+            continue
+        if min_salary is not None and job["min_salary"] < min_salary:
+            continue
+        if experience_yrs is not None and job["experience_yrs"] > experience_yrs:
+            continue
+        all_jobs.append(job)
+
+    if not all_jobs:
+        raise HTTPException(status_code=404, detail="No such job exists")
+
+    return all_jobs
+
+# 5th Endpoint -  View only remote jobs
+@app.get("/remote_jobs")
+def remote_jobs():
+    data = load_data()
+
+    remote_jobs_list = []
+    for job in data:
+        if job["remote"] == True:
+            remote_jobs_list.append(job)
+
+    if not remote_jobs_list:
+        raise HTTPException(status_code=404, detail="No remote jobs exists")
+
+    return remote_jobs_list
