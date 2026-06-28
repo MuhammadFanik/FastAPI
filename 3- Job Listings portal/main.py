ddbafb3058d4aa7005fcd851aaa645c04b82f478
Muhammad Fanik <muhammadfanik0@gmail.com>
2026-06-28 06:38:23 +0500

    Job Model complete

--- 3- Job Listings portal/main.py
+++ 3- Job Listings portal/main.py
@@ -9,3 +9,16 @@ app = FastAPI()
 class Requirements(BaseModel):
     degree: Annotated[str, Field(description="Degree required for this job")]
     skill: Annotated[List[str], Field(description="Skills required for this job")]
+
+# Model 2 - Jon
+class Job(BaseModel):
+    id: Annotated[str, Field(description="Job ID", gt=0)]
+    title: Annotated[str, Field(description="Job title of that person")]
+    company: Annotated[str, Field(description="Company name where the employee works")]
+    location: Annotated[str, Field(description="Location of the office")]
+    min_salary: Annotated[int, Field(description="Minimum salary an employee can have", ge=50000)]
+    max_salary: Annotated[int, Field(description="Maximum salary an employee can have", ge=1000000)]
+    experience_years: Annotated[int, Field(description="Number of experience years of an employee")]
+    remote: Annotated[bool, Field(description="Does the employee work remotely?")]
+    rating: float
+    requirements:Requirements
