313f2f0d7e8725b484a9a572d25025a44b84a6fd
Muhammad Fanik <muhammadfanik0@gmail.com>
2026-06-28 06:48:34 +0500

    Model validator added, computed field added, mistakes corrected

--- 3- Job Listings portal/main.py
+++ 3- Job Listings portal/main.py
@@ -8,17 +8,32 @@ app = FastAPI()
 # Model 1 - Requirements
 class Requirements(BaseModel):
     degree: Annotated[str, Field(description="Degree required for this job")]
-    skill: Annotated[List[str], Field(description="Skills required for this job")]
+    skills: Annotated[List[str], Field(description="Skills required for this job")]
 
 # Model 2 - Jon
 class Job(BaseModel):
-    id: Annotated[str, Field(description="Job ID", gt=0)]
+    id: Annotated[int, Field(description="Job ID", gt=0)]
     title: Annotated[str, Field(description="Job title of that person")]
     company: Annotated[str, Field(description="Company name where the employee works")]
     location: Annotated[str, Field(description="Location of the office")]
-    min_salary: Annotated[int, Field(description="Minimum salary an employee can have", ge=50000)]
-    max_salary: Annotated[int, Field(description="Maximum salary an employee can have", ge=1000000)]
-    experience_years: Annotated[int, Field(description="Number of experience years of an employee")]
+    min_salary: Annotated[float, Field(description="Minimum salary an employee can have", ge=50000)]
+    max_salary: Annotated[float, Field(description="Maximum salary an employee can have", ge=0)]
+    experience_yrs: Annotated[float, Field(description="Number of experience years of an employee")]
     remote: Annotated[bool, Field(description="Does the employee work remotely?")]
     rating: float
     requirements:Requirements
+
+
+    # Model validator on Job. Check min salary must always be less than max salary
+    @model_validator(mode="after")
+    def check_salary(self):
+        if self.min_salary > self.max_salary:
+            raise ValueError("Minimum salary cannot be greater than maximum salary")
+        else:
+            return self
+
+    # Adding a computed field - salary range: Add a computed field that returns a formatted string showing the salary range
+    @computed_field
+    @property
+    def salary_range(self) -> str:
+        return f"${self.min_salary} - ${self.max_salary}"
