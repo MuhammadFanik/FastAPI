8ca8e08a5783eb6e7d35423b33f04191e31779cf
Muhammad Fanik <muhammadfanik0@gmail.com>
2026-06-30 03:06:09 +0500

    Update customer endpoint created

--- 4- Contact Book API/main.py
+++ 4- Contact Book API/main.py
@@ -1,7 +1,7 @@
 from fastapi import  FastAPI, HTTPException, Path, Query
 import json
 import pickle
-from typing import Annotated, List, Dict
+from typing import Annotated, List, Dict, Optional
 from pydantic  import BaseModel, Field, field_validator, model_validator, EmailStr, computed_field
 from starlette.responses import JSONResponse
 
@@ -33,6 +33,13 @@ class Customer(BaseModel):
 
         # If everything goes right, we return the value
         return value
+
+
+class UpdateCustomer(BaseModel):
+    name: Annotated[Optional[str], Field(default=None)]
+    email: Annotated[Optional[EmailStr], Field(default=None)]
+    phone: Annotated[Optional[str], Field(default=None)]
+    city: Annotated[Optional[str], Field(default=None)]
 
 
 # Helper function to load data
@@ -99,3 +106,34 @@ def create_contact(customer: Customer, customer_id: str = Path(..., description=
 
     # Return success response
     return JSONResponse(status_code=201, content={"message": "Customer created successfully"})
+
+
+# Endpoint 5 - Update a contact - The user provides a contact ID and the new details, and you overwrite the existing record.
+# customer_id is the path parameter (which customer to update) and it is sent in the URL
+# update_customer is te request body (what fields to update) and UpdateCustomer is the pydantic model above. User has the option to change all or just a few details of himself
+@app.put("/update/{customer_id}")
+def update_customer(customer_id: str, update_customer:UpdateCustomer):
+    # Step 1: Load the data
+    data = load_data()
+
+    # Step 2: Check if customer exists in the database
+    if customer_id not in data:
+        raise HTTPException(status_code=404, detail="Customer is not in our database")
+
+    # Step 3: Get existing customer's current data
+    # Retrieve the data for the specific customer and this creates a reference, not a copy
+    existing_customer_data = data[customer_id]
+
+    # Step 4: Extract only the updated fields
+    # Only include fields that user actually sent in the request
+    # And this will convert from pydantic model to python Dict
+    updated_customer_info = update_customer.model_dump(exclude_unset=True)
+
+    # Step 5: Apply updates to existing data
+    for key, value in updated_customer_info.items():
+        existing_customer_data[key] = value
+
+    # Save the data
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={"message": "Customer updated successfully"})
