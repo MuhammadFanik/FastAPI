94d2f620bbe1799dfb83e89277f7f65e54a6f321
Muhammad Fanik <muhammadfanik0@gmail.com>
2026-06-30 00:40:50 +0500

    2 Endpoints, Pydantic model Complete

---
+++ 4- Contact Book API/main.py
@@ -0,0 +1,62 @@
+from fastapi import  FastAPI, HTTPException, Path, Query
+import json
+import pickle
+from typing import Annotated, List, Dict
+from pydantic  import BaseModel, Field, field_validator, model_validator, EmailStr, computed_field
+
+app = FastAPI()
+
+
+# Pydantic Model
+class Customer(BaseModel):
+    name: Annotated[str, Field(description="Customer Name")]
+    phone: Annotated[str, Field(description="Customer Phone")]
+    email: Annotated[EmailStr, Field(description="Customer Email")]
+    city: Annotated[str, Field(description="City where the customer lives")]
+
+    @field_validator("phone")
+    @classmethod
+    # A valid phone number can only have characters: 0-9,+,-
+    def phone_validation(cls, value):
+        # Checking if the phone has valid characters
+
+        # Allowed set of characters
+        allowed_chars = set("1234567890+- ")
+        for char in value:
+            if char not in allowed_chars:
+                raise ValueError("Character not allowed in phone number")
+
+        # Checking for the length of the phone
+        if len(value) < 7:
+            raise ValueError("Phone number too short")
+
+        # If everything goes right, we return the value
+        return value
+
+
+# Helper function to load data
+def load_data():
+    with open("data.json", "r") as f:
+        data = json.load(f)
+    return data
+
+# Save customer data to the JSON file. Converts python dictionary to JSON and write to file
+def save_data(data):    # Data is the dict containing all the customers
+    with open("data.json", "w") as f:
+        json.dump(data, f)
+
+
+
+
+# Endpoint 1 - Homepage endpoint
+@app.get("/")
+def home():
+    return {"message": "This is the home page of Customers Contact Book API "}
+
+# Endpoint 2 -  View all the customers data
+@app.get("/contacts")
+def view_all():
+    # Load the data
+    data = load_data()
+    # Return the data
+    return data
